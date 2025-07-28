9714f02eb957eeee5c9b8c4619987200af41b4ed
BLACK SHADOW <pubuduchinthaka558@gmail.com>
2025-07-28 22:14:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+from PIL import Image
+import pytesseract
+from flask_cors import CORS
+
+# ⚠️ මෙක Termux භාවිතා කරනවා නම් PATH කියන්න ඕනෙ
+pytesseract.pytesseract.tesseract_cmd = '/data/data/com.termux/files/usr/bin/tesseract'
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    file = request.files['image']
+    image = Image.open(file.stream)
+
+    # ⚠️ Sinhala Language OCR
+    extracted_text = pytesseract.image_to_string(image, lang='eng')
+
+    return jsonify({'extracted_text': extracted_text})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
